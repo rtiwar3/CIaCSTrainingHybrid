472bc4b26c4f2e9c0ace1baca407a9d8c44a24af
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-10-03 17:50:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -11,30 +11,22 @@ resource "azurerm_resource_group" "hybrid" {
   location = var.location_name
 }
 
-resource "azurerm_mysql_server" "hybrid" {
+resource "azurerm_mysql_flexible_server" "hybrid" {
   name                = var.mysql_server_name
-  location            = var.location_name
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.hybrid.location
+  resource_group_name = azurerm_resource_group.hybrid.name
 
   administrator_login          = var.mysql_server_login
   administrator_login_password = var.mysql_server_password
 
-  sku_name   = "GP_Gen5_2"
-  storage_mb = 5120
+  sku_name   = "B_Standard_B1s"
   version    = "5.7"
-
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = true
-  infrastructure_encryption_enabled = true
-  public_network_access_enabled     = false
-  ssl_enforcement_enabled           = false
 }
 
-resource "azurerm_mysql_database" "hybrid" {
-  name                = "exampledb"
-  resource_group_name = var.resource_group_name
-  server_name         = var.mysql_server_name
+resource "azurerm_mysql_flexible_database" "hybrid" {
+  name                = "shop_inventory"
+  resource_group_name = azurerm_resource_group.hybrid.name
+  server_name         = azurerm_mysql_flexible_server.hybrid.name
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
 }
