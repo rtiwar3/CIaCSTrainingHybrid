32edb71c2b8a30d2f5e5b0209b7710ce2e0b2709
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-10-03 14:32:26 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,18 @@
-output "website_address" {
-  description = "The Hostname associated with the App Service"
-  value       = format("https://%s/", azurerm_linux_web_app.webapp.default_hostname)
+#output "website_address" {
+#  description = "The Hostname associated with the App Service"
+#  value       = format("https://%s/", azurerm_linux_web_app.webapp.default_hostname)
+#}
+
+output "websiterul" {
+  value = format("%s%s", aws_instance.hybrid.public_ip, "/IMS")
 }
 
-output "db_hostname" {
-  value       = aws_db_instance.rds_instance.address
-  description = "Connect to the database at this endpoint"
-}
+#output "db_hostname" {
+#  value       = aws_db_instance.rds_instance.address
+#  description = "Connect to the database at this endpoint"
+#}
 
-output "port" {
-  value       = aws_db_instance.rds_instance.port
-  description = "The port the database is listening on"
-}
+#output "port" {
+#  value       = aws_db_instance.rds_instance.port
+#  description = "The port the database is listening on"
+#}
