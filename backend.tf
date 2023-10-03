687a42bdf771d2fc2013e347e247d67fa1a3bcbb
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-10-03 18:08:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -30,3 +30,11 @@ resource "azurerm_mysql_flexible_database" "hybrid" {
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
 }
+
+resource "azurerm_mysql_flexible_server_firewall_rule" "hybrid" {
+  name                = "hybrid"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_mysql_flexible_server.example.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
