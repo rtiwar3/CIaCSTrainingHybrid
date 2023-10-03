e758969dd5306bec16d4028010ff1eaf4843cdd4
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-10-03 23:22:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -11,32 +11,32 @@ resource "azurerm_resource_group" "hybrid" {
   location = var.location_name
 }
 
-resource "azurerm_mysql_flexible_server" "hybrid" {
+resource "azurerm_mysql_server" "hybrid" {
   name                = var.mysql_server_name
   location            = azurerm_resource_group.hybrid.location
   resource_group_name = azurerm_resource_group.hybrid.name
 
   administrator_login    = var.mysql_server_login
-  administrator_password = var.mysql_server_password
+  administrator_login_password = var.mysql_server_password
 
   sku_name   = "B_Standard_B1s"
   version    = "5.7"
 
-  require_secure_transport=OFF
+  ssl_enforcement_enabled = false
 }
 
-resource "azurerm_mysql_flexible_database" "hybrid" {
+resource "azurerm_mysql_database" "hybrid" {
   name                = "shop_inventory"
   resource_group_name = azurerm_resource_group.hybrid.name
-  server_name         = azurerm_mysql_flexible_server.hybrid.name
+  server_name         = azurerm_mysql_server.hybrid.name
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
 }
 
-resource "azurerm_mysql_flexible_server_firewall_rule" "hybrid" {
+resource "azurerm_mysql_server_firewall_rule" "hybrid" {
   name                = "hybrid"
   resource_group_name = azurerm_resource_group.hybrid.name
-  server_name         = azurerm_mysql_flexible_server.hybrid.name
+  server_name         = azurerm_mysql_server.hybrid.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "255.255.255.255"
 }
