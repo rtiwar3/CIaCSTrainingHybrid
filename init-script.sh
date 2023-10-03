2fab39c9ebd0c09a59b708c7c426eae0997d7659
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-10-03 16:11:51 +0530

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -2,18 +2,6 @@
 
 set -e
 
-sudo -s
-apt-get update
-apt-get install -y apache2
-systemctl enable apache2
-apt install php libapache2-mod-php php-mysql -y
-a2enmod rewrite
-systemctl restart apache2
-chmod 777 -R /etc/apache2/apache2.conf
-mv /etc/apache2/apache2.conf /etc/apache2/apache2_old.conf
-cp apache2.conf /etc/apache2/apache2.conf
-rm /etc/apache2/apache2_old.conf
-systemctl restart apache2
-
-git clone -b rds https://github.com/SmithaVerity/IMS.git
-mv IMS /var/www/html
+git clone -b rds https://github.com/SmithaVerity/IMSServerSetup.git
+cd IMSServerSetup/
+sh deployServer.sh
