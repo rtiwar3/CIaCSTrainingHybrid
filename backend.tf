f4ae012340382cb57f9a27881f5b601d80e3dca9
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-10-03 23:30:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -23,7 +23,8 @@ resource "azurerm_mysql_server" "hybrid" {
   storage_mb = 5120
   version    = "5.7"
 
-  ssl_enforcement_enabled = false
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
 resource "azurerm_mysql_database" "hybrid" {
