38ce32ab6c4d38b4cf5776844eda5c516c03658c
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-03 23:53:32 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -34,3 +34,10 @@ resource "azurerm_mysql_flexible_server_firewall_rule" "hybrid" {
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "255.255.255.255"
 }
+
+resource "azurerm_mysql_flexible_server_configuration" "parameters" {
+  name                = "require_secure_transport"
+  resource_group_name = azurerm_resource_group.hybrid.name
+  server_name         = azurerm_mysql_flexible_server.hybrid.name
+  value               = "OFF"
+}
